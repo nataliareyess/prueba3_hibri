c67c92a20dbda748fe670d5bfdd7475bdb1f1c7b
nataliareyess <148658946+nataliareyess@users.noreply.github.com>
2024-01-20 20:45:51 -0300

    Primer commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.prueba3hibrido',
+  appName: 'prueba3hibrido',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
